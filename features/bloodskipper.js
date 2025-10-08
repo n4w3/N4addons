28572f0e60e831284a98e4bc3f1629464c7bcf8b
n4w3 <naw3wan@gmail.com>
2025-10-08 08:46:23 +0200

    1.0.0

---
+++ features/bloodskipper.js
@@ -0,0 +1,45 @@
+const TARGET_NAME = "handle this"; 
+let wasPresent = false;
+
+register("tick", () => {
+    const found = World.getAllEntities().some(e => {
+        if (e.getClassName() !== "EntityArmorStand") return false;
+
+        const nbt = e.getNBT();
+        if (!nbt.getBoolean("CustomNameVisible")) return false;
+
+        let name = nbt.getString("CustomName");
+        if (!name) return false;
+
+        name = name.replace(/§./g, "");
+
+        return name.toLowerCase().includes(TARGET_NAME.toLowerCase());
+    });
+
+    if (wasPresent && !found) {
+        ChatLib.chat(`§4[N4] Kill Mobs!`);
+        showDisappearMessage();
+    }
+
+    wasPresent = found;
+});
+
+function showDisappearMessage() {
+    const message = `§4Kill Mobs!`;
+    let ticks = 0;
+
+    const renderTrigger = register("renderOverlay", () => {
+        if (ticks > 60) {
+            renderTrigger.unregister();
+            return;
+        }
+
+        Renderer.drawString(
+            message,
+            Renderer.screen.getWidth() / 2 - Renderer.getStringWidth(message) / 2,
+            Renderer.screen.getHeight() / 2
+        );
+
+        ticks++;
+    });
+}
